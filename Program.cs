2fc30c79e92a1fa808bb8e27d0f45fa8c2e02681
Phehello Semoko-Marope <ST10163295@rcconnect.edu.za>
2024-04-12 20:51:16 +0000

    My previous project did not open so i had to copy and paste. Hence why there arent any chages.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,125 @@ using System.Threading.Tasks;
 
 namespace PhehelloSemoko_PROG6221_Part1_ST10163295
 {
-    internal class Program
+    class fullRecipe
     {
-        static void Main(string[] args)
+        public String[] nameIngred { get; set; }
+        public String[] unitOfIngred { get; set; }
+        public int[] quantityOfIngred { get; set; }
+        public int numIngred { get; set; }
+        public int numOfSteps { get; set; }
+        public string[] listSteps { get; set; }
+        public string recipeName { get; set; }
+
+
+        public void createRecipe()
         {
+
+            Console.Write("Enter the name of the Recipe: ");
+            recipeName = Console.ReadLine();
+
+
+
+            Console.WriteLine("Please enter the amount of ingredients you'd like to add.");
+            numIngred = Convert.ToInt32(Console.ReadLine());
+
+            nameIngred = new string[numIngred];
+            quantityOfIngred = new int[numIngred];
+            unitOfIngred = new String[numIngred];
+
+            for (int i = 0; i < numIngred; i++)
+            {
+                Console.Write("Enter the name of the ingredient: ");
+                nameIngred[i] = Console.ReadLine();
+
+                Console.Write("Enter the Quantity: ");
+                quantityOfIngred[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the unit of measurement: ");
+                unitOfIngred[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter the amount of steps there are: ");
+            numOfSteps = Convert.ToInt32(Console.ReadLine());
+            listSteps = new string[numOfSteps];
+
+            for (int i = 0; i < numOfSteps; i++)
+            {
+                Console.Write("Enter the steps below: ");
+                listSteps[i] = Console.ReadLine();
+                        }
+            
+
+        }
+        public void exitMenu()
+        {
+            Environment.Exit(0);
+        }
+
+        public void showRecipe()
+        {
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine($"                      RECIPE NAME: {recipeName}");
+            Console.WriteLine("---------------------------------------------------------");
+
+            for (int i = 0; i < numIngred; i++)
+            {
+                Console.WriteLine($"{quantityOfIngred[i]} {unitOfIngred[i]} Of  {nameIngred[i]}");
+            }
+
+            Console.WriteLine("Here are the steps: ");
+
+            for (int i = 0; i < numOfSteps; i++)
+            {
+                Console.WriteLine($"{listSteps[i]}");
+            }
+
+        }
+
+
+        internal class Program
+        {
+
+            static void Main(string[] args)
+            {
+                fullRecipe myRecipe = new fullRecipe();
+                bool exit = false;
+
+
+               
+                while (!exit)
+                {
+                    Console.WriteLine("---------------------------------------------------------");
+                    Console.WriteLine("                      Recipe Maker");
+                    Console.WriteLine("---------------------------------------------------------");
+                    Console.WriteLine("");
+                    Console.WriteLine("Select one of the following menu items:");
+                    Console.WriteLine("(1) Create menu");
+                    Console.WriteLine("(2) Show Recipe");
+                    Console.WriteLine("(3)");
+                    Console.WriteLine("(3) Exit Menu");
+                    Console.WriteLine("");
+
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            myRecipe.createRecipe();
+                            break;
+
+                        case "2":
+                            myRecipe.showRecipe();
+                            break;
+
+                        case "3":
+                            myRecipe.exitMenu();
+                            break;
+                    }
+
+                }
+            
+
+            }
         }
     }
 }
